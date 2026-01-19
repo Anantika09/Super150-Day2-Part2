768fd85595f0bf5ed36b14638bbc13aadd1a10ca
Anantika Agarwal <anantikaag06@gmail.com>
2026-01-19 15:23:08 +0530

    added more information

--- src/main/java/com/example/day2_part2/controller/Home.java
+++ src/main/java/com/example/day2_part2/controller/Home.java
@@ -1,5 +1,5 @@
 package com.example.day2_part2.controller;
-
+import java.util.*;
 import com.example.day2_part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student=new StudentModel(1,"anantika","anantika@gmail.com");
-        return student;
+    public List<StudentModel> getStudent(){
+        List<StudentModel> students = new ArrayList<>();
+        students.add(new StudentModel(1,"anantika","anantika@gmail.com"));
+        students.add(new StudentModel(2,"khushi","khushi@gmail.com"));
+        students.add(new StudentModel(3,"ananya","ananya@gmail.com"));
+        students.add(new StudentModel(4,"ram","ram@gmail.com"));
+        students.add(new StudentModel(5,"shyam","shyam@gmail.com"));
+
+        return students;
+
+
     }
 }
